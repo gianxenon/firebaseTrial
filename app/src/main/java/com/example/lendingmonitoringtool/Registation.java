49c0a91cd028774b0c693417f905e906e26f03e8
gianxenon <54257471+gianxenon@users.noreply.github.com>
2021-03-09 22:54:59 +0800

    hi?

--- app/src/main/java/com/example/lendingmonitoringtool/Registation.java
+++ app/src/main/java/com/example/lendingmonitoringtool/Registation.java
@@ -1,5 +1,6 @@
 package com.example.lendingmonitoringtool;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -7,8 +8,11 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import java.io.IOException;
+import java.util.Calendar;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -18,12 +22,16 @@ public class Registation extends AppCompatActivity {
     private CircleImageView ProfileImage;
     private static final int PICK_IMAGE = 1;
     Uri imageUri;
+
+
+    EditText editTextDate ;
+    DatePickerDialog.OnDateSetListener onDateSetListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registation);
 
-
+         editTextDate  = findViewById(R.id.edt_bday);
 
         ProfileImage = (CircleImageView) findViewById(R.id.profile_image);
         ProfileImage.setOnClickListener(new View.OnClickListener() {
@@ -37,7 +45,28 @@ public class Registation extends AppCompatActivity {
                 startActivityForResult(Intent.createChooser(gallery, "Select Picture"), PICK_IMAGE);
             }
         });
+        editTextDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
 
+                DatePickerDialog datePickerDialog = new DatePickerDialog(Registation.this, onDateSetListener, year, month, day);
+
+                datePickerDialog.show();
+            }
+        });
+
+        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month+1;
+                String date = dayOfMonth + "/" + month + "/" + year;
+                editTextDate.setText(date);
+            }
+        };
 
     }
 
@@ -54,8 +83,11 @@ public class Registation extends AppCompatActivity {
                 ProfileImage.setImageBitmap(bitmap);
             } catch (IOException e) {
                 e.printStackTrace();
-            }
-        }
-
+            } }
     }
+
+
+
+
+
     }
